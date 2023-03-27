25aca865de994e19d6c554da47980f48fa59881e
zhuowenzhao <zwenzhao11@gmail.com>
2023-03-27 16:34:10 -0700

    no need for env varibale inputs

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+. .env
 docker cp data/dump mongodb:/dump
-docker exec -i mongodb /usr/bin/mongorestore --username $1 --password $2 --authenticationDatabase admin --db content_registry /dump/lbl-mlexchange
+docker exec -i mongodb /usr/bin/mongorestore --username $MONGO_DB_USERNAME --password $MONGO_DB_PASSWORD --authenticationDatabase admin --db content_registry /dump/lbl-mlexchange
