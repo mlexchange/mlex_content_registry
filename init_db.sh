3f59aa6d053b688aa40cf37e08ff2cbafe3ce594
zhuowenzhao <zwenzhao11@gmail.com>
2023-03-28 00:32:28 -0700

    updated init_db

--- init_db.sh
+++ init_db.sh
@@ -2,4 +2,4 @@
 
 . .env
 docker cp data/dump mongodb:/dump
-docker exec -i mongodb /usr/bin/mongorestore --username $MONGO_DB_USERNAME --password $MONGO_DB_PASSWORD --authenticationDatabase admin --db content_registry /dump/lbl-mlexchange
+docker exec -i mongodb /usr/bin/mongorestore --username=$MONGO_DB_USERNAME --password=$MONGO_DB_PASSWORD --authenticationDatabase=admin --db=content_registry --drop /dump/lbl-mlexchange
