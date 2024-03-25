6c898f43ad49c455f813b670e95e79ac1641fa16
Tibbers Hao <ghao@lbl.gov>
2024-03-25 14:31:20 -0700

    deleted init_db.sh as no longer used in docker-compose

--- init_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. .env
-docker cp data/dump mongodb:/dump
-docker exec -i mongodb /usr/bin/mongorestore --username=$MONGO_DB_USERNAME --password=$MONGO_DB_PASSWORD --authenticationDatabase=admin --db=content_registry --drop /dump/lbl-mlexchange
